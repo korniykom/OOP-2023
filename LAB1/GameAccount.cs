df98715ffa9f13383da4747bff4e4367b88347a0
korniykom <korniykom@gmail.com>
2023-09-26 21:34:21 +0300

    completed LoseGame and GetHistory methods

--- LAB1/GameAccount.cs
+++ LAB1/GameAccount.cs
@@ -8,18 +8,38 @@ namespace MyGame;
 public class GameAccount {
     private List<GameLogs> allGames = new List<GameLogs>();
     public string UserName { get; }
-    public decimal CurrentRating { get; }
-    public decimal GamesCount { get; }
+    public decimal CurrentRating { get; set; }
+    public decimal GamesCount { get; set; }
 
     public void WinGame(string opponentName, decimal rating) {
         if(rating < 1) {
             throw new ArgumentOutOfRangeException(nameof(rating), "Rating cannot be less than 1");
         }
-        var gameLog = new GameLogs(opponentName, rating);
+        CurrentRating += rating;
+        GamesCount++;
+        var gameLog = new GameLogs(opponentName, CurrentRating);
         allGames.Add(gameLog);
     }
     public void LoseGame(string opponentName, decimal rating) {
-        // to do
+        if(rating < 1) {
+            throw new ArgumentOutOfRangeException(nameof(rating), "Rating cannot be less than 1");
+        }
+        if(CurrentRating - rating < 1) {
+            CurrentRating = 1;
+        }
+        CurrentRating -= rating;
+        GamesCount++;
+        var gameLog = new GameLogs(opponentName, CurrentRating);
+        allGames.Add(gameLog);
+    }
+    public string GetHistory() {
+        var report = new System.Text.StringBuilder();
+        decimal won = 0;
+        decimal lost = 0;
+        foreach(var item in allGames) {
+            report.AppendLine($"________\n Player {UserName} had rating of {item.rating} point(s) after game against player {item.opponentName}");
+        }
+        return report.ToString();
     }
     //construct
     public GameAccount(string name) {
--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -3,6 +3,9 @@
 public class GameExample {
     static void Main() {
         var playa = new GameAccount("Max");
-        playa.WinGame("John", 200);
+        playa.WinGame("John", 300);
+        playa.LoseGame("Trace", 100);
+        System.Console.WriteLine(playa.GetHistory());
+        
     }
 }
