b3b9b5d7a3311612253ceeb2706550f1b6be451b
korniykom <korniykom@gmail.com>
2023-10-12 15:23:02 +0300

    LAB 1 finish

--- LAB1/GameAccount.cs
+++ LAB1/GameAccount.cs
@@ -3,78 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MyGame;
-
-public class GameAccount {
-    private List<GameLogs> allGames = new List<GameLogs>();
-    private decimal gameSeed = 100;
-    public string UserName { get; }
-    public decimal CurrentRating { get; set; }
-    public decimal GamesCount { get; set; }
-    public void WinGame(GameAccount opponent, decimal rating) {
-        if(rating < 1) {
-            throw new ArgumentOutOfRangeException(nameof(rating), "Rating cannot be less than 1");
-        }
-        decimal id = gameSeed;
-        gameSeed++;
-        if(opponent.CurrentRating - rating < 1) {
-            opponent.CurrentRating = 1;
-            CurrentRating += rating - opponent.CurrentRating;
-        } else {
-            opponent.CurrentRating = opponent.CurrentRating - rating;
-            CurrentRating += rating;
-        }
-        GamesCount++;
-        var gameLog = new GameLogs(opponent.UserName, CurrentRating, opponent.CurrentRating, true, rating, id);
-        allGames.Add(gameLog);
-        var opponentGameLog = new GameLogs(UserName, opponent.CurrentRating, CurrentRating, false, rating, id);
-        opponent.allGames.Add(opponentGameLog);
-    }
-    public void LoseGame(GameAccount opponent, decimal rating) {
-        if(rating < 1) {
-            throw new ArgumentOutOfRangeException(nameof(rating), "Rating cannot be less than 1");
-        }
-        if(CurrentRating - rating < 1) {
+namespace MyGame {
+    public class GameAccount {
+        private List<GameLogs> allGames = new List<GameLogs>();
+        //constructor
+        public GameAccount(string name) {
+            this.UserName = name;
             CurrentRating = 1;
-            opponent.CurrentRating += rating - CurrentRating;
-        } else {
-        CurrentRating -= rating;
-        opponent.CurrentRating += rating;
         }
-        decimal id = gameSeed;
-        gameSeed++;
-        GamesCount++;
-        var gameLog = new GameLogs(opponent.UserName, CurrentRating, opponent.CurrentRating, false, rating, id);
-        allGames.Add(gameLog);
-
-        var opponentGameLog = new GameLogs(UserName, opponent.CurrentRating, CurrentRating, true, rating, id);
-        opponent.allGames.Add(opponentGameLog);
-    }
-    public string GetStats() {
-        System.Console.WriteLine("Stats of " + UserName);
-        var report = new System.Text.StringBuilder();
-        var outcome = "Won";
-        decimal Rate = 0;
-        report.AppendLine("|--------|----------|------------|--------------|-------|----|------|");
-        report.AppendLine("|UserName|UserRating|OpponentName|OpponentRating|Outcome|Rate|GameID|");
-        report.AppendLine("|--------|----------|------------|--------------|-------|----|------|");
-        foreach(var item in allGames) {
-            if(item.outcome) {
-                outcome = "Won";
-                Rate = item.winRate;
-            } else {
-                outcome = "Lost";
-                Rate = -item.winRate;
+        private string UserName;
+        private int _currentRating;
+        public int CurrentRating {
+            get {return _currentRating;}
+            set {
+                if(value < 1) {
+                    _currentRating = 1;
+                } else {
+                    _currentRating = value;
+                }
             }
-            report.AppendLine(String.Format("|{0,8}|{1,10}|{2,12}|{3,14}|{4,7}|{5,4}|{6,6}|", UserName, item.rating, item.opponentName,item.OpponentRating, outcome, Rate, item.GameID));
-            report.AppendLine("|--------|----------|------------|--------------|-------|----|------|");
         }
-        return report.ToString();
-    }
-    //construct
-    public GameAccount(string name) {
-        this.UserName = name;
-        this.GamesCount = 0;
-        this.CurrentRating = 1;
+        private int GamesCount { get { return allGames.Count;} }
+        public void GameResult(GameAccount opponent, int rating, string outcome) {
+            if(rating < 1) {
+                throw new ArgumentOutOfRangeException(nameof(rating), "Rating cannot be less than 1");
+            }
+            if(opponent != this) {
+                if(outcome == "Win") {
+                    this.WinGame(opponent, rating);
+                    opponent.LoseGame(this, rating);
+                    // GameLogs.gameIDSeed++;
+                } else if(outcome == "Lose") {
+                    this.LoseGame(opponent, rating);
+                    opponent.WinGame(this, rating);
+                    // GameLogs.gameIDSeed++;
+                } else {
+                    throw new ArgumentException("You should pass correct outcome");
+                }
+            } else {
+                throw new ArgumentException("Player should play against other player");
+            }
+        }
+        private void WinGame(GameAccount opponent, int rating) {
+            CurrentRating += rating;
+
+           // var gameLog = ;
+            allGames.Add(new GameLogs(UserName, CurrentRating, opponent.UserName, opponent.CurrentRating, "Win", GameLogs.gameIDSeed, rating));
+        }
+        private void LoseGame(GameAccount opponent, int rating) {
+            CurrentRating -= rating;
+            
+            //var gameLog = ;
+            allGames.Add(new GameLogs(UserName, CurrentRating, opponent.UserName, opponent.CurrentRating, "Lose", GameLogs.gameIDSeed, rating));
+        }
+        public string GetStats() {
+            System.Console.WriteLine("Stats of " + UserName);
+            var report = new System.Text.StringBuilder();
+
+            report.AppendLine("|--------|----------|------------|--------------|-------|------|---|");
+            report.AppendLine("|UserName|UserRating|OpponentName|OpponentRating|Outcome|GameID|Bet|");
+            report.AppendLine("|--------|----------|------------|--------------|-------|------|---|");
+            
+            foreach(var item in allGames) {
+                report.AppendLine(String.Format("|{0,8}|{1,10}|{2,12}|{3,14}|{4,7}|{5,6}|{6,3}|", item.playerName, item.playerRating, item.opponentName,item.opponentRating, item.outcome, item.gameID, item.GameBet));
+                report.AppendLine("|--------|----------|------------|--------------|-------|------|---|");
+            }
+            return report.ToString();
+        }
     }
 }
--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -1,23 +1,23 @@
-﻿namespace MyGame;
+﻿namespace MyGame {
+    public class GameExample {
+        static void Main() {
+            var player1 = new GameAccount("Max");
+            var player2 = new GameAccount("Frank");
+            var player3 = new GameAccount("Sonya");
 
-public class GameExample {
-    static void Main() {
-        var player1 = new GameAccount("Max");
-        var player2 = new GameAccount("Frank");
-        
-        
-        player1.WinGame(player2, -100);
-        player1.LoseGame(player2, 50);
-        player1.WinGame(player2, 20);
-       
-        player1.WinGame(player2, 550);
-        player1.LoseGame(player2, 300);
-        player1.WinGame(player2, 150);
-        
-        
-        
-        System.Console.WriteLine(player1.GetStats());
-        System.Console.WriteLine(player2.GetStats());
-   
+            player1.GameResult(player2, 100, "Win");
+            player1.GameResult(player2, 200, "Win");
+
+            player2.GameResult(player1, 10, "Win");
+            player2.GameResult(player1, 10, "Win");
+
+            player1.GameResult(player3, 40, "Lose");
+            player3.GameResult(player2, 100, "Win");
+
+            System.Console.WriteLine(player1.GetStats());
+            System.Console.WriteLine(player2.GetStats());
+            System.Console.WriteLine(player3.GetStats());
+            
+        }
     }
 }
